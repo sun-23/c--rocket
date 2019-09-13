30e70bf5cfc645c9b871e53d81ec76e38e574a3b
Chaichana <49665324+sun-23@users.noreply.github.com>
2019-09-13 13:45:07 +0700

    Add files via upload

---
+++ rocket_chaichana/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace rocket_chaichana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 326);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(423, 325);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 50;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(229, 261);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progressBar1.Maximum = 200;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(208, 19);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(560, 266);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(521, 266);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Score";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 20;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 50;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 90;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 50;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::rocket_chaichana.Properties.Resources.rocket2;
+            this.pictureBox3.Location = new System.Drawing.Point(34, 266);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 67);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::rocket_chaichana.Properties.Resources.rocket1;
+            this.pictureBox2.Location = new System.Drawing.Point(586, 182);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(92, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::rocket_chaichana.Properties.Resources.rocket;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 157);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::rocket_chaichana.Properties.Resources.space;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(726, 358);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ rocket_chaichana/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rocket_chaichana
+{
+    public partial class Form1 : Form
+    {
+        int cnt = 1;
+        int y1;
+        int y2;
+        int y3;
+
+        int h;
+        Random r;
+
+        int score;
+        bool start;
+
+        int status ;
+        public Form1()
+        {
+            InitializeComponent();
+            y1 = pictureBox1.Top;
+            y2 = pictureBox2.Top;
+
+            h = Height;
+            r = new Random();
+
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
+            pictureBox3.Enabled = false;
+            start = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left = pictureBox1.Left + 10;
+            if (pictureBox1.Left > Width)
+            {
+                pictureBox1.Left = -pictureBox1.Width;
+                pictureBox1.Top = r.Next(h - pictureBox1.Height * 2);
+                y1 = pictureBox1.Top;
+
+                scoreCal(true, 1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer3.Enabled = true;
+            timer5.Enabled = true;
+
+            pictureBox1.Enabled = true;
+            pictureBox2.Enabled = true;
+            pictureBox3.Enabled = true;
+            score = 100;
+            label1.Text = score.ToString();
+            progressBar1.Value = 100;
+            start = true;
+
+            pictureBox1.Load(@"images\rocket.jpg");
+            pictureBox1.Left = -pictureBox1.Width;
+            pictureBox2.Load(@"images\rocket1.jpg");
+            pictureBox2.Left = +pictureBox1.Width;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            pictureBox1.Load(@"images\BOMB.jpg");
+            int w2 = pictureBox1.Height;
+            pictureBox1.Top = y1 - w2 / 2;
+            timer2.Enabled = true;
+
+            scoreCal(true, 2);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (cnt % 2 == 1)
+            {
+                pictureBox1.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+            }
+            cnt++;
+            if (cnt == 10)
+            {
+                pictureBox1.Load(@"images\rocket.jpg");
+
+                pictureBox1.Left = -pictureBox1.Width;
+                pictureBox1.Top = r.Next(h - pictureBox1.Height * 2);
+                y1 = pictureBox1.Top;
+
+                pictureBox1.Visible = true;
+
+                timer2.Enabled = false;
+                timer1.Enabled = true;
+                cnt = 0;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();     
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            status = 0;
+            scoreCal(start,status);
+        }
+
+        void scoreCal(bool startbool,int status)
+        {
+            if (startbool)
+            {
+                if (status == 0)
+                {
+                    score = score - 5;
+                    label1.Text = score.ToString();
+                    if (score <= 0)
+                    {
+                        progressBar1.Value = 0;
+                    }
+                    else
+                    {
+                        progressBar1.Value = score;
+                    }
+                    if (score <= 0)
+                    {
+                        timer1.Enabled = false;
+                        timer2.Enabled = false;
+                        timer3.Enabled = false;
+                        timer4.Enabled = false;
+                        timer5.Enabled = false;
+                        timer6.Enabled = false;
+                        MessageBox.Show("Game Orver!!!");
+                    }
+                }
+                else if (status == 1)
+                {
+                    score = score - 15;
+                    label1.Text = score.ToString();
+                    if (score <= 0)
+                    {
+                        progressBar1.Value = 0;
+                    }
+                    else
+                    {
+                        progressBar1.Value = score;
+                    }
+                    if (score <= 0)
+                    {
+                        timer1.Enabled = false;
+                        timer2.Enabled = false;
+                        timer3.Enabled = false;
+                        timer4.Enabled = false;
+                        timer5.Enabled = false;
+                        timer6.Enabled = false;
+                        MessageBox.Show("Game Orver!!!");
+                    }
+                }
+                else
+                {
+                    score = score + 10;
+                    if (score <= 200)
+                    {
+                        progressBar1.Value = score;
+                    }
+                    else
+                    {
+                        progressBar1.Value = 200;
+                    }
+                    label1.Text = score.ToString();
+                    if (score >= 200)
+                    {
+                        timer1.Enabled = false;
+                        timer2.Enabled = false;
+                        timer3.Enabled = false;
+                        timer4.Enabled = false;
+                        timer5.Enabled = false;
+                        timer6.Enabled = false;
+                        MessageBox.Show("Win");
+                    }
+                }
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+             pictureBox2.Left = pictureBox2.Left - 5;
+            if (pictureBox2.Left <= -pictureBox3.Width  )
+            {
+                pictureBox2.Left = Width ;
+                pictureBox2.Top = r.Next(h - pictureBox2.Height * 2);
+                y2 = pictureBox2.Top;
+
+                scoreCal(true, 1);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+            pictureBox2.Load(@"images\BOMB.jpg");
+            int w2 = pictureBox2.Height;
+            pictureBox2.Top = y2 - w2 / 2;
+            timer4.Enabled = true;
+
+            scoreCal(true, 2);
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (cnt % 2 == 1)
+            {
+                pictureBox2.Visible = false;
+            }
+            else
+            {
+                pictureBox2.Visible = true;
+            }
+            cnt++;
+            if (cnt == 10)
+            {
+                pictureBox2.Load(@"images\rocket1.jpg");
+
+                pictureBox2.Left = +Width;
+                pictureBox2.Top = r.Next(h - pictureBox2.Height * 2);
+                y2 = pictureBox2.Top;
+
+                pictureBox2.Visible = true;
+
+                timer4.Enabled = false;
+                timer3.Enabled = true;
+                cnt = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            pictureBox3.Left = pictureBox3.Left + 10;
+            pictureBox3.Top = pictureBox3.Top - 5;
+            if (pictureBox3.Top < -pictureBox3.Height || pictureBox3.Left > Width)
+            {
+                pictureBox3.Top = Height;
+                pictureBox3.Left = r.Next(Width - pictureBox3.Width * 2);
+
+                scoreCal(true, 1);
+            }
+
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            if (cnt % 2 == 1)
+            {
+                pictureBox3.Visible = false;
+            }
+            else
+            {
+                pictureBox3.Visible = true;
+            }
+            cnt++;
+            if (cnt == 10)
+            {
+                pictureBox3.Load(@"images\rocket2.jpg");
+
+                pictureBox3.Left = -pictureBox2.Width;
+                pictureBox3.Top = r.Next(h - pictureBox3.Height * 2);
+                y3 = pictureBox2.Top;
+
+                pictureBox3.Visible = true;
+
+                timer6.Enabled = false;
+                timer5.Enabled = true;
+                cnt = 0;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            timer5.Enabled = false;
+            pictureBox2.Load(@"images\BOMB.jpg");
+            int w2 = pictureBox2.Height;
+            pictureBox3.Top = Height;
+            timer6.Enabled = true;
+
+            scoreCal(true, 2);
+        }
+
+    }
+}
